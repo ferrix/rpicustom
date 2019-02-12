e0b6624dce6f1a02f4ca8c75a4b2e990233c5784
Ferrix Hovi <ferrix@ferrix.fi>
2019-02-12 09:56:28 +0200

    Fix docker image name

--- buildenv.sh
+++ buildenv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
 
 ./getdeps.sh
-docker run --privileged=true -it -v $(pwd):/root imagebuilder /root/mangle.sh
+docker run --privileged=true -it -v $(pwd):/root ubuntu /root/mangle.sh
